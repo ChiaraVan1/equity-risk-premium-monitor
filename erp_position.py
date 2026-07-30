bda69bb131839c34ed7acd082c398984eda8530f
ChiaraVan1 <102356801+ChiaraVan1@users.noreply.github.com>
2026-07-30 20:22:58 +0800

    Clean up erp_position.py and fix import
    Removed unnecessary lines and corrected import statement.

--- erp_position.py
+++ erp_position.py
@@ -1,16 +1,11 @@
-稀需业)
-i"930598": "低估-资源",
-    ("980032", "新能电池")
-mport pandas as pd
+import pandas as pd
 import numpy as np
 import os
 import time
 from datetime import datetime
 import requests
 import akshare as ak
-930598
 
-    "980032": "新能-电池",
 from etf_metrics import load_etf_metrics, build_etf_metrics_block, ERP_TO_ETF
 from popularity_signal import build_popularity_block, compute_popularity_confirmation
 
